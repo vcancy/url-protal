060ecfff9b210b9219fdcecdcab0674f4fc9519e
yujie <yujie@guoshengtianfeng.com>
2018-02-09 15:12:05 +0800

    任务时间默认当前时间可修改

--- rws/models.py
+++ rws/models.py
@@ -7,16 +7,17 @@
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from datetime import datetime
+import django.utils.timezone as timezone
 class TDatareportConfig(models.Model):
     task = models.CharField(max_length=255, verbose_name="任务编号")
     sqltext = models.TextField(max_length=1000, verbose_name="sql脚本")
     attach = models.IntegerField(verbose_name="是否添加附件excel.1-是，0-否",default=1)
     receivers = models.CharField(max_length=1000, verbose_name="接受人邮箱，多个已逗号','分隔",default="xxx@guoshengtianfeng.com")
-    dbinfo = models.TextField(max_length=255, verbose_name='数据库',default='{"host":"192.168.103.201","user":"root","pwd":"Wxb123456!","port":3306,"db":"report"}')
+    dbinfo = models.TextField(max_length=255, verbose_name='数据库',default='{"host":"10.165.124.186","user":"root","pwd":"2dd7ce4c5a3144ea520f89b1ec61fa2b","port":3306,"db":"report"}')
     title = models.CharField(max_length=255, blank=False, null=True, verbose_name="邮件标题")
     updatetime = models.DateTimeField(blank=True, null=False, verbose_name="更新时间",auto_now = True)
     enable = models.IntegerField(blank=True, null=False,verbose_name="是否启用，0-否，1-启用",default=1)
-    schedule = models.DateTimeField(blank=True, null=False,verbose_name="任务执行时间")
+    schedule = models.DateTimeField(blank=True, null=False,verbose_name="任务执行时间",default = timezone.now)
     class Meta:
         verbose_name = 'rws-定时邮件发送任务'
         verbose_name_plural = 'rws-定时邮件发送任务'
