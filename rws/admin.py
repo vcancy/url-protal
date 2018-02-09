65e70fa2c4a999ba3395c3fb41eee91c5b5f98c5
yujie <yujie@guoshengtianfeng.com>
2018-02-09 15:30:34 +0800

    1.添加列表显示字段 2.添加搜索

--- rws/admin.py
+++ rws/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import TDatareportConfig
 # Register your models here.
 
+class rwsAdmin(admin.ModelAdmin):
+    list_display = ('task','title','enable','schedule','receivers') #添加字段显示
+    search_fields = ('task','title') #添加快速查询栏
 
-
-admin.site.register(TDatareportConfig)
+admin.site.register(TDatareportConfig,rwsAdmin)
--- urlboard/admin.py
+++ urlboard/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import URL,Group
 # Register your models here.
-admin.site.register(URL)
+class urlAdmin(admin.ModelAdmin):
+    list_display = ('name','group','url','desc') #添加字段显示
+    search_fields = ('name') #添加快速查询栏
+admin.site.register(URL,urlAdmin)
 admin.site.register(Group)
