353a46489e6ab3db50e0644855ac6c352000cd34
yujie <yujie@guoshengtianfeng.com>
2018-02-09 15:35:31 +0800

    1.添加列表显示字段 2.添加搜索

--- urlboard/admin.py
+++ urlboard/admin.py
@@ -3,6 +3,6 @@ from .models import URL,Group
 # Register your models here.
 class urlAdmin(admin.ModelAdmin):
     list_display = ('name','group','url','desc') #添加字段显示
-    search_fields = ('name') #添加快速查询栏
+    search_fields = ('name',) #添加快速查询栏
 admin.site.register(URL,urlAdmin)
 admin.site.register(Group)
